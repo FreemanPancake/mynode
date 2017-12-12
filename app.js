fba066adc74f83a30677597734be5e999ef8765b
Freeman <869147578@qq.com>
2017-12-12 17:00:07 +0800

    test database connection

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//add this for database connection
+var mysql = require('mysql');
+//end database requirement
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+//set database connection
+app.dbconnect = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'test_database'
+})
+//end set database connection
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var app = require('../app');
 var router = express.Router();
+
+var string = app.dbconnect.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
