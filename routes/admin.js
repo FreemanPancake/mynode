1879b8dff6b0171818f56418197d0b1bded8a54d
Freeman <869147578@qq.com>
2017-12-15 13:41:18 +0800

    add admin function

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET admin listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('this is admin page');
-// });
+router.get('/', function(req, res, next) {
+  res.send('this is admin page');
+});
 
 /* GET admin home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: title });
-  });
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: title });
+//   });
 
 
 module.exports = router;
