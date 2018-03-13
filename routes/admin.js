bf91b3874028a0f79b4f09ef87efe6889977db32
Freemanysz <869147578@qq.com>
2018-03-13 13:35:36 +0800

    add admin page

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET admin listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('this is admin page');
+// });
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('this is admin page');
+  res.render('admin', { title: "this is a admin page" });
 });
 
 /* GET admin home page. */
